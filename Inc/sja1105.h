8c500556e4dbbf812230b63c4d64733d459c3c7b
Ben <bens15351@gmail.com>
2025-07-29 19:12:29 +0100

    Added port configuration

--- Inc/sja1105.h
+++ Inc/sja1105.h
@@ -21,6 +21,10 @@ extern "C" {
 
 
 /* Defines */
+
+#define SJA1105_NUM_PORTS (5)
+
+/* Control frame */
 #define SJA1105_SPI_WRITE_FRAME      (1 << 31)     /* 1 in the most significant bit signifies a write */
 #define SJA1105_SPI_READ_FRAME       (0)           /* 0 in the most significant bit signifies a write */
 #define SJA1105_SPI_MAX_PAYLOAD_SIZE (64)          /* 2080 bits / 32 = 65 frames (minus 1 for the control frame) */
@@ -29,65 +33,136 @@ extern "C" {
 #define SJA1105_SPI_SIZE_MASK        (0x0000003f)  /* 6-bit size */
 #define SJA1105_SPI_SIZE_POSITION    (25)          /* occupies bits[30:25] */
 
-#define SJA1105_STATIC_CONF_ADDR 0x20000
+/* Timings */
+#define SJA1105_T_RST                (5000)        /* 5000ns (5us) */
+#define SJA1105_T_SPI_WR             (130)         /* ns */
+#define SJA1105_T_SPI_CTRL_DATA      (64)          /* Time between writing the command frame and reading data in ns */
+#define SJA1105_T_SPI_LEAD           (40)          /* ns */
+#define SJA1105_T_SPI_LAG            (40)          /* ns */
+
+
+/* ---------------------------------------------------------------------------- */
+/* Auxiliary Configurations Unit */
+/* ---------------------------------------------------------------------------- */
+
+#define SJA1105_ACU_REG_CFG_PAD_MIIX_BASE      (0x100800)
+#define SJA1105_ACU_REG_CFG_PAD_MIIX_TX(port)  (SJA1105_ACU_REG_CFG_PAD_MIIX_BASE + 2 * port)
+#define SJA1105_ACU_REG_CFG_PAD_MIIX_RX(port)  (SJA1105_ACU_REG_CFG_PAD_MIIX_BASE + 2 * port + 1)
+#define SJA1105_ACU_REG_CFG_PAD_MIIX_ID(port)  (SJA1105_ACU_REG_CFG_PAD_MIIX_BASE + 16 + port)
+
+#define SJA1105_ACU_REG_CFG_PAD_MISC           (0x100840)
+#define SJA1105_ACU_REG_CFG_PAD_SPI            (0x100880)
+#define SJA1105_ACU_REG_CFG_PAD_JTAG           (0x100881)
+
+#define SJA1105_ACU_REG_PORT_STATUS_MIIX_BASE  (0x100900)
+#define SJA1105_ACU_REG_PORT_STATUS_MIIX(port) (SJA1105_ACU_REG_PORT_STATUS_MIIX_BASE + port)
+
+#define SJA1105_ACU_REG_TS_CONFIG              (0x100a00)
+#define SJA1105_ACU_REG_TS_STATUS              (0x100a01)
+#define SJA1105_ACU_REG_PROD_CFG               (0x100bc0)
+#define SJA1105_ACU_REG_PROD_ID                (0x100bc3)
+#define SJA1105_ACU_REG_ACCESS_DISABLE         (0x100bfd)
+
+
+/* ---------------------------------------------------------------------------- */
+/* Clock Generation Unit */
+/* ---------------------------------------------------------------------------- */
+
+
+
+
+
+#define SJA1105_STATIC_CONF_ADDR (0x20000)
+
+
+
 
 /* Typedefs */
 
 typedef enum {
-  SJA1105_OK              = HAL_OK,
-  SJA1105_ERROR           = HAL_ERROR,
-  SJA1105_BUSY            = HAL_BUSY,
-  SJA1105_TIMEOUT         = HAL_TIMEOUT,
-  SJA1105_PARAMETER_ERROR
+	SJA1105_OK              = HAL_OK,
+	SJA1105_ERROR           = HAL_ERROR,
+	SJA1105_BUSY            = HAL_BUSY,
+	SJA1105_TIMEOUT         = HAL_TIMEOUT,
+	SJA1105_PARAMETER_ERROR
 } SJA1105_StatusTypeDef;
 
 typedef enum {
-  VARIANT_SJA1105  = 0x00,
-  VARIANT_SJA1105T = 0x01,
-  VARIANT_SJA1105P = 0x02,
-  VARIANT_SJA1105Q = 0x03,
-  VARIANT_SJA1105R = 0x04,
-  VARIANT_SJA1105S = 0x05
+	VARIANT_SJA1105  = 0x00,
+	VARIANT_SJA1105T = 0x01,
+	VARIANT_SJA1105P = 0x02,
+	VARIANT_SJA1105Q = 0x03,
+	VARIANT_SJA1105R = 0x04,
+	VARIANT_SJA1105S = 0x05
 } SJA1105_VariantTypeDef;
 
-typedef void                  (*SJA1105_CallbackDelayMSTypeDef)     (uint32_t ms);
-typedef SJA1105_StatusTypeDef (*SJA1105_CallbackTakeMutexTypeDef)   (uint32_t timeout);
-typedef SJA1105_StatusTypeDef (*SJA1105_CallbackGiveMutexTypeDef)   (void);
+typedef enum {
+	SJA1105_INTERFACE_MII  = HAL_ETH_MII_MODE,
+	SJA1105_INTERFACE_RMII = HAL_ETH_RMII_MODE,
+	SJA1105_INTERFACE_GMII,
+	SJA1105_INTERFACE_RGMII,
+	SJA1105_INTERFACE_SGMII
+} SJA1105_InterfaceTypeDef;
+
+typedef enum {
+	SJA1105_SPEED_UNKNOWN,  /* Used for ports that need to negotiate a speed */
+	SJA1105_SPEED_10M,
+	SJA1105_SPEED_100M,
+	SJA1105_SPEED_1G,
+} SJA1105_SpeedTypeDef;
 
 typedef struct {
-    SJA1105_CallbackDelayMSTypeDef   callback_delay_ms;
-    SJA1105_CallbackTakeMutexTypeDef callback_take_mutex;
-    SJA1105_CallbackGiveMutexTypeDef callback_give_mutex;
+	uint8_t                  port_num;
+	SJA1105_SpeedTypeDef     speed;
+	SJA1105_InterfaceTypeDef interface;
+	bool                     configured;
+} SJA1105_PortTypeDef;
+
+
+typedef void                  (*SJA1105_CallbackDelayMSTypeDef)   (uint32_t ms);
+typedef void                  (*SJA1105_CallbackDelayNSTypeDef)   (uint32_t ns);
+typedef SJA1105_StatusTypeDef (*SJA1105_CallbackTakeMutexTypeDef) (uint32_t timeout);
+typedef SJA1105_StatusTypeDef (*SJA1105_CallbackGiveMutexTypeDef) (void);
+
+typedef struct {
+	SJA1105_CallbackDelayMSTypeDef   callback_delay_ms;  /* Non-blocking delay in ms */
+	SJA1105_CallbackDelayNSTypeDef   callback_delay_ns;  /* Blocking delay in ns */
+	SJA1105_CallbackTakeMutexTypeDef callback_take_mutex;
+	SJA1105_CallbackGiveMutexTypeDef callback_give_mutex;
 } SJA1105_CallbacksTypeDef;
 
 typedef struct {
-    SJA1105_VariantTypeDef          variant;
-    const SJA1105_CallbacksTypeDef *callbacks;
-    SPI_HandleTypeDef              *spi_handle;  /* SPI Handle */
-    GPIO_TypeDef                   *cs_port;
-    uint16_t                        cs_pin;
-    GPIO_TypeDef                   *rst_port;
-    uint16_t                        rst_pin;
-    uint32_t                        timeout;  /* Timeout in ms for doing anything with a timeout (read, write, take mutex etc) */
-    bool                            initialised;
+	SJA1105_VariantTypeDef          variant;
+	const SJA1105_CallbacksTypeDef *callbacks;
+	SPI_HandleTypeDef              *spi_handle;  /* SPI Handle */
+	GPIO_TypeDef                   *cs_port;
+	uint16_t                        cs_pin;
+	GPIO_TypeDef                   *rst_port;
+	uint16_t                        rst_pin;
+	uint32_t                        timeout;  /* Timeout in ms for doing anything with a timeout (read, write, take mutex etc) */
+	bool                            initialised;
+	SJA1105_PortTypeDef            *ports;
 } SJA1105_HandleTypeDef;
 
 
 /* Functions */
 
 /* Initialisation */
+SJA1105_StatusTypeDef SJA1105_ConfigurePort(SJA1105_PortTypeDef *ports, uint8_t port_num, SJA1105_InterfaceTypeDef interface, SJA1105_SpeedTypeDef speed);
+
 SJA1105_StatusTypeDef SJA1105_Init(
-    SJA1105_HandleTypeDef *dev,
-    const SJA1105_VariantTypeDef variant,
-    const SJA1105_CallbacksTypeDef* callbacks,
-    SPI_HandleTypeDef *spi_handle,
-    GPIO_TypeDef *cs_port,
-    uint16_t cs_pin,
-    GPIO_TypeDef *rst_port,
-    uint16_t rst_pin,
-    uint32_t timeout,
-    const uint32_t *static_conf,
-    const uint32_t static_conf_size
+		SJA1105_HandleTypeDef *dev,
+		const SJA1105_VariantTypeDef variant,
+		const SJA1105_CallbacksTypeDef *callbacks,
+		SPI_HandleTypeDef *spi_handle,
+		GPIO_TypeDef *cs_port,
+		uint16_t cs_pin,
+		GPIO_TypeDef *rst_port,
+		uint16_t rst_pin,
+		uint32_t timeout,
+		const uint32_t *static_conf,
+		uint32_t static_conf_size,
+		SJA1105_PortTypeDef *ports
 );
 
 /* User Functions */
--- Src/sja1105.c
+++ Src/sja1105.c
@@ -10,68 +10,92 @@
 #define CONSTRAIN(amt,low,high) ((amt)<(low)?(low):((amt)>(high)?(high):(amt)))
 
 
+/* ---------------------------------------------------------------------------- */
 /* Initialisation */
+/* ---------------------------------------------------------------------------- */
+
+SJA1105_StatusTypeDef SJA1105_ConfigurePort(SJA1105_PortTypeDef *ports, uint8_t port_num, SJA1105_InterfaceTypeDef interface, SJA1105_SpeedTypeDef speed){
+
+	SJA1105_StatusTypeDef status = SJA1105_OK;
+
+	/* Check the parameters */
+	if (port_num >= SJA1105_NUM_PORTS)       status = SJA1105_PARAMETER_ERROR;
+	if (interface > SJA1105_INTERFACE_SGMII) status = SJA1105_PARAMETER_ERROR;
+	if (speed > SJA1105_SPEED_1G)            status = SJA1105_PARAMETER_ERROR;
+	if (ports[port_num].configured)          status = SJA1105_ERROR;
+	if (status != SJA1105_OK) return status;
+
+	/* Assign the parameters */
+	ports[port_num].port_num   = port_num;
+	ports[port_num].interface  = interface;
+	ports[port_num].speed      = speed;
+	ports[port_num].configured = true;
+
+	return status;
+}
+
 SJA1105_StatusTypeDef SJA1105_Init(
-    SJA1105_HandleTypeDef *dev,
-    const SJA1105_VariantTypeDef variant,
-    const SJA1105_CallbacksTypeDef *callbacks,
-    SPI_HandleTypeDef *spi_handle,
-    GPIO_TypeDef *cs_port,
-    uint16_t cs_pin,
-    GPIO_TypeDef *rst_port,
-    uint16_t rst_pin,
-    uint32_t timeout,
-    const uint32_t *static_conf,
-    uint32_t static_conf_size){
+		SJA1105_HandleTypeDef *dev,
+		const SJA1105_VariantTypeDef variant,
+		const SJA1105_CallbacksTypeDef *callbacks,
+		SPI_HandleTypeDef *spi_handle,
+		GPIO_TypeDef *cs_port,
+		uint16_t cs_pin,
+		GPIO_TypeDef *rst_port,
+		uint16_t rst_pin,
+		uint32_t timeout,
+		const uint32_t *static_conf,
+		uint32_t static_conf_size,
+		SJA1105_PortTypeDef *ports){
 
-    SJA1105_StatusTypeDef status = SJA1105_OK;
+	SJA1105_StatusTypeDef status = SJA1105_OK;
 
-    dev->variant     = variant;
-    dev->callbacks   = callbacks;
-    dev->spi_handle  = spi_handle;
-    dev->cs_port     = cs_port;
-    dev->cs_pin      = cs_pin;
-    dev->rst_port    = rst_port;
-    dev->rst_pin     = rst_pin;
-    dev->timeout     = timeout;
-    dev->initialised = false;
+	dev->variant     = variant;
+	dev->callbacks   = callbacks;
+	dev->spi_handle  = spi_handle;
+	dev->cs_port     = cs_port;
+	dev->cs_pin      = cs_pin;
+	dev->rst_port    = rst_port;
+	dev->rst_pin     = rst_pin;
+	dev->timeout     = timeout;
+	dev->initialised = false;
 
-    /* Only the SJA1105Q has been implemented. TODO: Add more */
-    if (dev->variant != VARIANT_SJA1105Q) status = SJA1105_PARAMETER_ERROR;
+	/* Only the SJA1105Q has been implemented. TODO: Add more */
+	if (dev->variant != VARIANT_SJA1105Q) status = SJA1105_PARAMETER_ERROR;
 
-    /* Check SPI parameters */
-    if (dev->spi_handle->Init.DataSize != SPI_DATASIZE_32BIT)  status = SJA1105_PARAMETER_ERROR;
-    if (dev->spi_handle->Init.CLKPolarity != SPI_POLARITY_LOW) status = SJA1105_PARAMETER_ERROR;
-    if (dev->spi_handle->Init.CLKPhase != SPI_PHASE_1EDGE)     status = SJA1105_PARAMETER_ERROR;
-    if (dev->spi_handle->Init.NSS != SPI_NSS_SOFT)             status = SJA1105_PARAMETER_ERROR;
-    if (dev->spi_handle->Init.FirstBit != SPI_FIRSTBIT_MSB)    status = SJA1105_PARAMETER_ERROR;
+	/* Check SPI parameters */
+	if (dev->spi_handle->Init.DataSize != SPI_DATASIZE_32BIT)  status = SJA1105_PARAMETER_ERROR;
+	if (dev->spi_handle->Init.CLKPolarity != SPI_POLARITY_LOW) status = SJA1105_PARAMETER_ERROR;
+	if (dev->spi_handle->Init.CLKPhase != SPI_PHASE_1EDGE)     status = SJA1105_PARAMETER_ERROR;
+	if (dev->spi_handle->Init.NSS != SPI_NSS_SOFT)             status = SJA1105_PARAMETER_ERROR;
+	if (dev->spi_handle->Init.FirstBit != SPI_FIRSTBIT_MSB)    status = SJA1105_PARAMETER_ERROR;
 
-    /* If there are invalid parameters then return */
-    if (status != SJA1105_OK) return status;
+	/* If there are invalid parameters then return */
+	if (status != SJA1105_OK) return status;
 
-    /* Set pins to a known state */
-    HAL_GPIO_WritePin(dev->rst_port, dev->rst_pin, SET);
-    HAL_GPIO_WritePin(dev->cs_port, dev->cs_pin, SET);
-
-
-    /* Configure the SJA1105 following the steps from UM11040 figure 2 */
-
-    /* Step 1: RESET */
-    SJA1105_Reset(dev);
-
-    /* Step 2: ACU REGISTER SETUP */
-
-    /* Step 3: CGU REGISTER SETUP */
-
-    /* Step 4: SGMII PHY/PCS (optional) */
-
-    /* Step 5: STATIC CONFIGURATION */
-    SJA1105_WriteRegister(dev, SJA1105_STATIC_CONF_ADDR, static_conf, static_conf_size);
+	/* Set pins to a known state */
+	HAL_GPIO_WritePin(dev->rst_port, dev->rst_pin, SET);
+	HAL_GPIO_WritePin(dev->cs_port, dev->cs_pin, SET);
 
 
-    dev->initialised = true;
+	/* Configure the SJA1105 following the steps from UM11040 figure 2 */
 
-    return status;
+	/* Step 1: RESET */
+	SJA1105_Reset(dev);
+
+	/* Step 2: ACU REGISTER SETUP */
+
+	/* Step 3: CGU REGISTER SETUP */
+
+	/* Step 4: SGMII PHY/PCS (optional) */
+
+	/* Step 5: STATIC CONFIGURATION */
+	SJA1105_WriteRegister(dev, SJA1105_STATIC_CONF_ADDR, static_conf, static_conf_size);
+
+
+	dev->initialised = true;
+
+	return status;
 }
 
 
@@ -80,117 +104,127 @@ SJA1105_StatusTypeDef SJA1105_Init(
 /* Low-Level Functions */
 SJA1105_StatusTypeDef SJA1105_ReadRegister(SJA1105_HandleTypeDef *dev, uint32_t addr, uint32_t *data, uint32_t size){
 
-    SJA1105_StatusTypeDef status = SJA1105_OK;
+	SJA1105_StatusTypeDef status = SJA1105_OK;
 
-    /* TODO: If a read follows a write to the same register then insert a 130ns delay here */
+	/* TODO: If a read follows a write to the same register then insert a 130ns delay here */
 
-    /* Check the parameters */
-    if (size == 0)                              status = SJA1105_PARAMETER_ERROR;  /* Empty check */
-    if (addr & ~SJA1105_SPI_ADDR_MASK)          status = SJA1105_PARAMETER_ERROR;  /* Start address check */
-    if ((addr + size) & ~SJA1105_SPI_ADDR_MASK) status = SJA1105_PARAMETER_ERROR;  /* End address check */
-    if (status != SJA1105_OK) return status;
+	/* Check the parameters */
+	if (size == 0)                              status = SJA1105_PARAMETER_ERROR;  /* Empty check */
+	if (addr & ~SJA1105_SPI_ADDR_MASK)          status = SJA1105_PARAMETER_ERROR;  /* Start address check */
+	if ((addr + size) & ~SJA1105_SPI_ADDR_MASK) status = SJA1105_PARAMETER_ERROR;  /* End address check */
+	if (status != SJA1105_OK) return status;
 
-    /* Take the mutex */
-    status = dev->callbacks->callback_take_mutex(dev->timeout);
-    if (status != SJA1105_OK) return status;
+	/* Take the mutex */
+	status = dev->callbacks->callback_take_mutex(dev->timeout);
+	if (status != SJA1105_OK) return status;
 
-    /* Initialise counter for the number of double words remaining to receive */
-    uint32_t dwords_remaining = size;
-    
-    /* If the number of double words to read is greater than SJA1105_SPI_MAX_PAYLOAD_SIZE, then the read needs to be broken into smaller transactions */
-    do {
+	/* Initialise counter for the number of double words remaining to receive */
+	uint32_t dwords_remaining = size;
 
-        /* Create the command frame */    
-        uint32_t command_frame = SJA1105_SPI_READ_FRAME;
-        command_frame |= ((addr + size - dwords_remaining) & SJA1105_SPI_ADDR_MASK) << SJA1105_SPI_ADDR_POSITION;
-        command_frame |= (CONSTRAIN(dwords_remaining, 0, SJA1105_SPI_MAX_PAYLOAD_SIZE) & SJA1105_SPI_SIZE_MASK) << SJA1105_SPI_SIZE_POSITION;  /* Note that if the read size = SPI_MAX_PAYLOAD_SIZE it will wrap to 0 as intended */
+	/* If the number of double words to read is greater than SJA1105_SPI_MAX_PAYLOAD_SIZE, then the read needs to be broken into smaller transactions */
+	do {
 
-        /* Start the transaction */
-        HAL_GPIO_WritePin(dev->cs_port, dev->cs_pin, RESET);
-        
-        /* Send command frame */
-        if (HAL_SPI_Transmit(dev->spi_handle, (uint8_t *) &command_frame, 1, dev->timeout) != HAL_OK){
-            status = SJA1105_ERROR;
-            return status;
-        }
-        
-        /* Receive payload */
-        if (HAL_SPI_Receive(dev->spi_handle, (uint8_t *) &data[size - dwords_remaining], CONSTRAIN(dwords_remaining, 0, SJA1105_SPI_MAX_PAYLOAD_SIZE), dev->timeout) != HAL_OK){
-            status = SJA1105_ERROR;
-            return status;
-        }
+		/* Create the command frame */
+		uint32_t command_frame = SJA1105_SPI_READ_FRAME;
+		command_frame |= ((addr + size - dwords_remaining) & SJA1105_SPI_ADDR_MASK) << SJA1105_SPI_ADDR_POSITION;
+		command_frame |= (CONSTRAIN(dwords_remaining, 0, SJA1105_SPI_MAX_PAYLOAD_SIZE) & SJA1105_SPI_SIZE_MASK) << SJA1105_SPI_SIZE_POSITION;  /* Note that if the read size = SPI_MAX_PAYLOAD_SIZE it will wrap to 0 as intended */
 
-        /* End the transaction */
-        HAL_GPIO_WritePin(dev->cs_port, dev->cs_pin, SET);
-        
-        /* Calculate the double words to receive remaining */
-        dwords_remaining -= CONSTRAIN(dwords_remaining, 0, SJA1105_SPI_MAX_PAYLOAD_SIZE);
+		/* Start the transaction after a delay (ensures successive transactions meet timing requirements) */
+		dev->callbacks->callback_delay_ns(SJA1105_T_SPI_WR);
+		HAL_GPIO_WritePin(dev->cs_port, dev->cs_pin, RESET);
+		dev->callbacks->callback_delay_ns(SJA1105_T_SPI_LEAD);
 
-    } while (dwords_remaining > 0);
+		/* Send command frame */
+		if (HAL_SPI_Transmit(dev->spi_handle, (uint8_t *) &command_frame, 1, dev->timeout) != HAL_OK){
+			status = SJA1105_ERROR;
+			return status;
+		}
 
-    /* Give the mutex */
-    status = dev->callbacks->callback_give_mutex();
-    if (status != SJA1105_OK) return status;
+		/* Insert delay to allow the device to fetch the data */
+		dev->callbacks->callback_delay_ns(SJA1105_T_SPI_CTRL_DATA);
 
-    return status;
+		/* Receive payload */
+		if (HAL_SPI_Receive(dev->spi_handle, (uint8_t *) &data[size - dwords_remaining], CONSTRAIN(dwords_remaining, 0, SJA1105_SPI_MAX_PAYLOAD_SIZE), dev->timeout) != HAL_OK){
+			status = SJA1105_ERROR;
+			return status;
+		}
+
+		/* End the transaction */
+		dev->callbacks->callback_delay_ns(SJA1105_T_SPI_LAG);
+		HAL_GPIO_WritePin(dev->cs_port, dev->cs_pin, SET);
+
+		/* Calculate the double words to receive remaining */
+		dwords_remaining -= CONSTRAIN(dwords_remaining, 0, SJA1105_SPI_MAX_PAYLOAD_SIZE);
+
+	} while (dwords_remaining > 0);
+
+	/* Give the mutex */
+	status = dev->callbacks->callback_give_mutex();
+	if (status != SJA1105_OK) return status;
+
+	return status;
 }
 
 SJA1105_StatusTypeDef SJA1105_WriteRegister(SJA1105_HandleTypeDef *dev, uint32_t addr, const uint32_t *data, uint32_t size){
 
-    SJA1105_StatusTypeDef status = SJA1105_OK;
+	SJA1105_StatusTypeDef status = SJA1105_OK;
 
-    /* Check the parameters */
-    if (size == 0)                              status = SJA1105_PARAMETER_ERROR;  /* Empty check */
-    if (addr & ~SJA1105_SPI_ADDR_MASK)          status = SJA1105_PARAMETER_ERROR;  /* Start address check */
-    if ((addr + size) & ~SJA1105_SPI_ADDR_MASK) status = SJA1105_PARAMETER_ERROR;  /* End address check */
-    if (status != SJA1105_OK) return status;
+	/* Check the parameters */
+	if (size == 0)                              status = SJA1105_PARAMETER_ERROR;  /* Empty check */
+	if (addr & ~SJA1105_SPI_ADDR_MASK)          status = SJA1105_PARAMETER_ERROR;  /* Start address check */
+	if ((addr + size) & ~SJA1105_SPI_ADDR_MASK) status = SJA1105_PARAMETER_ERROR;  /* End address check */
+	if (status != SJA1105_OK) return status;
 
-    /* Take the mutex */
-    status = dev->callbacks->callback_take_mutex(dev->timeout);
-    if (status != SJA1105_OK) return status;
+	/* Take the mutex */
+	status = dev->callbacks->callback_take_mutex(dev->timeout);
+	if (status != SJA1105_OK) return status;
 
-    /* Initialise counter for the number of double words remaining to transmit */
-    uint32_t dwords_remaining = size;
-    
-    /* If the payload size is greater than SJA1105_SPI_MAX_PAYLOAD_SIZE, then the write needs to be broken into smaller transactions */
-    do {
+	/* Initialise counter for the number of double words remaining to transmit */
+	uint32_t dwords_remaining = size;
 
-        /* Create the command frame */    
-        uint32_t command_frame = SJA1105_SPI_WRITE_FRAME;
-        command_frame |= ((addr + size - dwords_remaining) & SJA1105_SPI_ADDR_MASK) << SJA1105_SPI_ADDR_POSITION;
+	/* If the payload size is greater than SJA1105_SPI_MAX_PAYLOAD_SIZE, then the write needs to be broken into smaller transactions */
+	do {
 
-        /* Start the transaction */
-        HAL_GPIO_WritePin(dev->cs_port, dev->cs_pin, RESET);
+		/* Create the command frame */
+		uint32_t command_frame = SJA1105_SPI_WRITE_FRAME;
+		command_frame |= ((addr + size - dwords_remaining) & SJA1105_SPI_ADDR_MASK) << SJA1105_SPI_ADDR_POSITION;
 
-        /* Send command frame */
-        if (HAL_SPI_Transmit(dev->spi_handle, (uint8_t *) &command_frame, 1, dev->timeout) != HAL_OK){
-            status = SJA1105_ERROR;
-            return status;
-        }
-        
-        /* Send payload */
-        if (HAL_SPI_Transmit(dev->spi_handle, (uint8_t *) &data[size - dwords_remaining], CONSTRAIN(dwords_remaining, 0, SJA1105_SPI_MAX_PAYLOAD_SIZE), dev->timeout) != HAL_OK){
-            status = SJA1105_ERROR;
-            return status;
-        }
-    
-        /* End the transaction */
-        HAL_GPIO_WritePin(dev->cs_port, dev->cs_pin, SET);
+		/* Start the transaction after a delay (ensures successive transactions meet timing requirements) */
+		dev->callbacks->callback_delay_ns(SJA1105_T_SPI_WR);
+		HAL_GPIO_WritePin(dev->cs_port, dev->cs_pin, RESET);
+		dev->callbacks->callback_delay_ns(SJA1105_T_SPI_LEAD);
 
-        /* Calculate the double words to transmit remaining */
-        dwords_remaining -= CONSTRAIN(dwords_remaining, 0, SJA1105_SPI_MAX_PAYLOAD_SIZE);
+		/* Send command frame */
+		if (HAL_SPI_Transmit(dev->spi_handle, (uint8_t *) &command_frame, 1, dev->timeout) != HAL_OK){
+			status = SJA1105_ERROR;
+			return status;
+		}
 
-    } while (dwords_remaining > 0);
+		/* Send payload */
+		if (HAL_SPI_Transmit(dev->spi_handle, (uint8_t *) &data[size - dwords_remaining], CONSTRAIN(dwords_remaining, 0, SJA1105_SPI_MAX_PAYLOAD_SIZE), dev->timeout) != HAL_OK){
+			status = SJA1105_ERROR;
+			return status;
+		}
 
-    /* Give the mutex */
-    status = dev->callbacks->callback_give_mutex();
-    if (status != SJA1105_OK) return status;
+		/* End the transaction */
+		dev->callbacks->callback_delay_ns(SJA1105_T_SPI_LAG);
+		HAL_GPIO_WritePin(dev->cs_port, dev->cs_pin, SET);
 
-    return status;
+		/* Calculate the double words to transmit remaining */
+		dwords_remaining -= CONSTRAIN(dwords_remaining, 0, SJA1105_SPI_MAX_PAYLOAD_SIZE);
+
+	} while (dwords_remaining > 0);
+
+	/* Give the mutex */
+	status = dev->callbacks->callback_give_mutex();
+	if (status != SJA1105_OK) return status;
+
+	return status;
 }
 
 void SJA1105_Reset(SJA1105_HandleTypeDef *dev){
-    HAL_GPIO_WritePin(dev->rst_port, dev->rst_pin, RESET);
-    dev->callbacks->callback_delay_ms(1);
-    HAL_GPIO_WritePin(dev->rst_port, dev->rst_pin, SET);
+	HAL_GPIO_WritePin(dev->rst_port, dev->rst_pin, RESET);
+	dev->callbacks->callback_delay_ns(SJA1105_T_RST);  /* 5us delay */
+	HAL_GPIO_WritePin(dev->rst_port, dev->rst_pin, SET);
+	dev->callbacks->callback_delay_ms(1);  /* 329us minimum until SPI commands can be written. Use a 1ms non-blocking delay so the RTOS can do other work */
 }
